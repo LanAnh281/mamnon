7d23364cbfa7ae3bca1d16815946319c940e1098
LanAnh <anh626801@gmail.com>
2024-03-02 22:01:07 +0700

    user

--- Backend/app/controller/schoolMedia.controller.js
+++ Backend/app/controller/schoolMedia.controller.js
@@ -1,5 +1,8 @@
 const { schoolMedia } = require("../models/index");
-const ApiError = require("../api-error")
+const ApiError = require("../api-error");
+const fs = require("fs");
+const path = require("path");
+const uploadDir = "./static/images";
 exports.create = async (req, res, next) => {
     const { schoolId } = req.body;
     console.log("File:", req.files, req.files.length)
@@ -35,11 +38,23 @@ exports.findAll = async (req, res, next) => {
 
 exports.delete = async (req, res, next) => {
     try {
+        const image = await schoolMedia.findOne({
+            where: {
+                _id: req.params.id
+            }
+        })
+        let fileImage = `${uploadDir}/${image.name}`;
+        console.log("F:", fileImage);
+        if (fs.existsSync(fileImage)) {
+            fs.unlinkSync(fileImage); //delete file
+            console.log(">>>Destroy:");
+        }
         const document = await schoolMedia.destroy({
             where: {
                 _id: req.params.id,
             },
         });
+
         return res.json({ message: document, status: "success" });
     } catch (error) {
         console.log(error);
